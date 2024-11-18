0a79619e132f5520190f0ca8a88a35852a2fcb8d
Bhumika Sharma <sharmabhumika54@gmail.com>
2024-11-18 18:33:32 +0530

    Booking Request file is working

--- TabBarControllerPawPal/BookingRequestViewController.swift
+++ TabBarControllerPawPal/BookingRequestViewController.swift
@@ -137,13 +137,15 @@ class BookingRequestViewController: UIViewController {
                 present(homeVC, animated: true)
             }
         }
+    
+    
     /*
-     // MARK: - Navigation
+      MARK: - Navigation
      
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
+      In a storyboard-based application, you will often want to do a little preparation before navigation
      override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
+      Get the new view controller using segue.destination.
+      Pass the selected object to the new view controller.
      }
      */
     
