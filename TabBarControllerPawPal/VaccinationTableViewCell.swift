6330db1571e4c4fc7f30a3e62a2cf74c50255c0d
Bhumika Sharma <sharmabhumika54@gmail.com>
2024-11-19 14:56:32 +0530

    feat : Final Merge

---
+++ TabBarControllerPawPal/VaccinationTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  VaccinationTableViewCell.swift
+//  PawPal_PetDetails
+//
+//  Created by admin19 on 18/11/24.
+//
+
+import UIKit
+
+class VaccinationTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var vaccineNameLabel: UILabel!
+    
+    @IBOutlet weak var vaccineTypeLabel: UILabel!
+    
+    @IBOutlet weak var dateOfVaccineLabel: UILabel!
+    
+    @IBOutlet weak var expiaryDateLabel: UILabel!
+    
+    
+    @IBOutlet weak var nextDueDateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TabBarControllerPawPal/vaccinationData.swift
@@ -0,0 +1,20 @@
+//
+//  vaccinationData.swift
+//  PawPal_PetDetails
+//
+//  Created by admin19 on 18/11/24.
+//
+
+import Foundation
+
+struct Vaccination : Codable {
+    var vaccineName: String
+    var vaccineType: String
+    var dateOfVaccination: String
+    var expiryDate: String
+    var nextDueDate: String
+}
+
+var vaccinations: [Vaccination] = []
+
+
