d7104f306c75708917721820266427f4d76d388d
Bhumika Sharma <sharmabhumika54@gmail.com>
2024-12-05 21:07:22 +0530

    Refactor: all the data model added in on file

--- TabBarControllerPawPal/DataModel/Caretaker Model.swift
+++ TabBarControllerPawPal/DataModel/Caretaker Model.swift
@@ -69,3 +69,68 @@ var bookings: [Booking] = [
     
 ]
 
+struct Vaccination : Codable {
+    var vaccineName: String
+    var vaccineType: String
+    var dateOfVaccination: String
+    var expiryDate: String
+    var nextDueDate: String
+}
+
+var vaccinations: [Vaccination] = []
+
+
+class PetData
+
+{
+    var petImage: String
+    var petName: String
+    var petBreed: String
+    var petGender: String
+    var petAge: String?
+    var petWeight : String?
+    
+    init(petImage: String, petName: String, petBreed: String, petGender: String, petAge: String , petWeight : String) {
+        self.petImage = petImage
+        self.petName = petName
+        self.petBreed = petBreed
+        self.petGender = petGender
+        self.petAge  = petAge
+        self.petWeight = petWeight
+    }
+}
+
+struct VaccinationDetails {
+    var vacinationName: String
+    var vacinationDate: String
+    var vacinationDescription: String
+    var isVaccinationCompleted: Bool
+}
+
+var vaccinationDetails: [VaccinationDetails] = [
+    VaccinationDetails(vacinationName: "Rabies", vacinationDate: "16 Jul, 2024", vacinationDescription: "All Doses Completed", isVaccinationCompleted: true),
+    VaccinationDetails(vacinationName: "Distemper Vaccine", vacinationDate: "18 Jul, 2024", vacinationDescription: "Booster Dose Needed", isVaccinationCompleted: false),
+    VaccinationDetails(vacinationName: "Parvovirus Vaccine", vacinationDate: "20 Jul, 2024", vacinationDescription: "All Doses Completed", isVaccinationCompleted: true),
+]
+
+class CaretakerData
+{
+   
+    var name: String
+    var price: String
+    var address: String
+    var rating: String
+    var isverified: Bool
+    var caretakerImage: String
+    
+    init(name: String, price: String, address: String, rating: String, isverified: Bool, caretakerImage: String)
+    {
+        self.name = name
+        self.price = price
+        self.address = address
+        self.rating = rating
+        self.isverified = isverified
+        self.caretakerImage = caretakerImage
+    }
+    
+}
--- TabBarControllerPawPal/DataModel/vaccinationData.swift
+++ TabBarControllerPawPal/DataModel/vaccinationData.swift
@@ -7,14 +7,14 @@
 
 import Foundation
 
-struct Vaccination : Codable {
-    var vaccineName: String
-    var vaccineType: String
-    var dateOfVaccination: String
-    var expiryDate: String
-    var nextDueDate: String
-}
-
-var vaccinations: [Vaccination] = []
+//struct Vaccination : Codable {
+//    var vaccineName: String
+//    var vaccineType: String
+//    var dateOfVaccination: String
+//    var expiryDate: String
+//    var nextDueDate: String
+//}
+//
+//var vaccinations: [Vaccination] = []
 
 
