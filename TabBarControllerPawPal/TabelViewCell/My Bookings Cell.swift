32dae1b01473b5e1ee9fe0c4441ecb00eae44623
skylancer76 <pawanbulusu@gmail.com>
2024-12-31 00:23:42 +0530

    Feat : My Bookings Page

---
+++ TabBarControllerPawPal/TabelViewCell/My Bookings Cell.swift
@@ -0,0 +1,23 @@
+//
+//  My Bookings Cell.swift
+//  TabBarControllerPawPal
+//
+//  Created by admin19 on 31/12/24.
+//
+
+import UIKit
+
+class My_Bookings_Cell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
